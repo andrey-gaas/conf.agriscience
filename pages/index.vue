99d66aae12e6fece71c8bc43182809014a2d1f9b
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2020-12-17 14:09:41 +0700

    develop branch

--- pages/index.vue
+++ pages/index.vue
@@ -1,26 +1,7 @@
 <template>
   <div class="container">
     <div>
-      <Logo />
-      <h1 class="title">conf.agriscience</h1>
-      <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--green"
-        >
-          Documentation
-        </a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          rel="noopener noreferrer"
-          class="button--grey"
-        >
-          GitHub
-        </a>
-      </div>
+      <h1>CONF AGRISCIENCE</h1>
     </div>
   </div>
 </template>
