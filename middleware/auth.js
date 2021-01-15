975f6240bfc1eb099230d263448ffe205017cb9d
andrey <captain.destroyer@yandex.ru>
2021-01-15 22:35:23 +0700

    auth middleware

--- middleware/auth.js
+++ middleware/auth.js
@@ -1,21 +1,8 @@
-const jwt = require('jsonwebtoken');
-const { JWT } = require('../constants/auth');
 
 module.exports = (req, res, next) => {
-  const authHeader = req.headers.authorization;
-  if (!authHeader) {
-    return res.status(401).send('Token not provided!');
+  if (req.isAuthenticated()) {
+    next();
+  } else {
+    return res.status(401).send('Unauthenticated');
   }
-
-  const token = authHeader.replace('Bearer ', '');
-  
-  try {
-    jwt.verify(token, JWT);
-  } catch (error) {
-    if (error instanceof jwt.JsonWebTokenError) {
-      res.status(401).send('Invalid token!');
-    }
-  }
-
-  next();
 };
