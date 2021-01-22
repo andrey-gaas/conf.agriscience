9112521fcb18d489d0c2f67a090cb506fd315a96
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2021-01-22 18:14:55 +0700

    post and put request for report

--- components/Header-empty.vue
+++ components/Header-empty.vue
@@ -48,7 +48,7 @@ export default {
     setLocale,
     localeRout,
     testFetch() {
-      this.$axios.get('/api/reports/1')
+      this.$axios.put('/api/reports/2', { title: 'Докладище' })
         .then(res => console.log(res))
         .catch(error => console.log(error.response.data));
     }
