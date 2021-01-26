0aa5689732ca40647bf1d60ef1887dc78aedb841
Гаас Андрей Александрович <Gaas@gpntbsib.ru>
2021-01-27 01:22:49 +0700

    Email confirm

--- api/auth.js
+++ api/auth.js
@@ -3,6 +3,7 @@ const passport = require('passport');
 const bcrypt = require('bcryptjs');
 const Mongo = require('./db/Mongo');
 const sendMail = require('./sendmail');
+const e = require('express');
 const router = Router();
 
 router.post('/registration', (req, res) => {
@@ -49,8 +50,8 @@ router.post('/registration', (req, res) => {
         else {
           const message = {
             email,
-            subject: 'Регистрация',
-            text: 'Регистрация пользователя',
+            subject: 'Регистрация - III Международный библиографический конгресс',
+            text: `Для подтверждения электронной почты, перейдите по ссылке: http://localhost:3000/api/auth/email-confirm/${email}`,
           };
           sendMail(message)
             .then(() => res.send('OK'))
@@ -65,5 +66,38 @@ router.post('/registration', (req, res) => {
 });
 
 router.post('/login', passport.authenticate('local', { successRedirect: '/personarea', failureRedirect: '/login' }));
+
+router.get('/email-confirm/:email', (req, res) => {
+  const { email } = req.params;
+
+  const users = Mongo.database
+    .db('bibcongress')
+    .collection('users');
+
+
+  users
+    .findOne({ email })
+    .then(user => {
+      if (!user) {
+        res.status(404).send('User not fount');
+      } else if (user.isEmailConfirmed) {
+        res.status(400).send('Email confirmed');
+      } else {
+        users
+          .findOneAndUpdate({ email }, { $set: { isEmailConfirmed: true } })
+          .then(() => {
+            res.redirect('/');
+          })
+          .catch(error => {
+            console.log(error.message);
+            res.status(500).redirect('/');
+          });
+      }
+    })
+    .catch(error => {
+      console.log(error.message);
+      res.status(500).send('Server Error');
+    });
+});
 
 module.exports = router;
