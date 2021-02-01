17e9b5d5fb1aa2429e924d3e20546a2c171c3f1f
Aleksey-Nuzhdin <alekseynuzdin@gmail.com>
2021-02-01 16:55:52 +0700

    fix client-onli

--- layouts/EmptyLayout.vue
+++ layouts/EmptyLayout.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="empty_layout_wrap min-h-100">
+      <client-only>
     <Header />
     <div class="d-flex align-items-center blue lighten-5 min-h-100 flex-grow-1 justify-content-center">
       <div class="spinner-border text-primary" style="width: 4rem; height: 4rem;" role="status"
@@ -7,10 +8,9 @@
       >
         <span class="sr-only">Loading...</span>
       </div>
-      <client-only>
         <Nuxt />
-      </client-only>
     </div>
+      </client-only>
   </div>
 </template>
 
